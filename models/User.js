c7bcda7e472f858d7a0a532a995ed4cd2a8ced5d
nengiamoni <amonifavour46@gmail.com>
2025-06-14 21:06:44 +0100

    changes

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
-
+ 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String },
@@ -8,11 +8,11 @@ const userSchema = new mongoose.Schema({
 }, {
   timestamps: true
 });
-
+ 
 // Hash the password before saving (only if modified)
 userSchema.pre('save', async function (next) {
   if (!this.isModified('password') || !this.password) return next();
-
+ 
   try {
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
@@ -21,11 +21,13 @@ userSchema.pre('save', async function (next) {
     next(err);
   }
 });
-
+ 
 // Compare entered password with hashed password
 userSchema.methods.comparePassword = function (candidatePassword) {
   if (!this.password) return false; // Prevent comparing if password is null (Google users)
   return bcrypt.compare(candidatePassword, this.password);
 };
-
+ 
 module.exports = mongoose.model('User', userSchema);
+ 
+ 
